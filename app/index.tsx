7378d3e43eeff5619abdbc2d1432ccce308ea4f7
PhatBormey <phatbormey@gmail.com>
2025-08-14 12:05:30 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { Text, View ,StyleSheet} from "react-native";
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+  
+}
+const colors={
+  primary:'#ffffff',
+  primary1:'#333333',
+  secondary:'#706C61',
+  secondary1:'#E1F4F3',
+}
+const styles=StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor: colors.primary,
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    text:{
+      color:colors.primary1,
+      fontSize:20,
+      fontWeight:'bold',
+      fontFamily:'times new roman'
+    }
+  });
+ 
